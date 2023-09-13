c416037a3931a3e3a835b3a17c57aae4e3507dcc
shubhamdarekar1998 <sdarekar662@gmail.com>
2023-09-13 20:55:46 +0530

    first commit

---
+++ aboutus.php
@@ -0,0 +1,133 @@
+<?php
+session_start();
+?>
+
+<html>
+
+  <head>
+    <title> About | Desi Meals' </title>
+	<meta charset="UTF-8">
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/aboutus.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body>
+
+  
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+  
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Desi Meals'</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li class="active"><a href="aboutus.php">About</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+          </ul>
+
+<?php
+if(isset($_SESSION['login_user1'])){
+
+?>
+
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user1']; ?> </a></li>
+            <li><a href="myrestaurant.php">MANAGER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+<?php
+}
+else if (isset($_SESSION['login_user2'])) {
+  ?>
+           <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user2']; ?> </a></li>
+            <li><a href="foodlist.php"><span class="glyphicon glyphicon-cutlery"></span> Food Zone </a></li>
+            <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart 
+            (<?php
+              if(isset($_SESSION["cart"])){
+              $count = count($_SESSION["cart"]); 
+              echo "$count"; 
+            }
+              else
+                echo "0";
+              ?>)
+            </a></li>
+            <li><a href="logout_u.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+  <?php        
+}
+else {
+
+  ?>
+
+<ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="customersignup.php"> User Sign-up</a></li>
+              <li> <a href="managersignup.php"> Manager Sign-up</a></li>
+          
+            </ul>
+            </li>
+
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+              <li> <a href="customerlogin.php"> User Login</a></li>
+              <li> <a href="managerlogin.php"> Manager Login</a></li>
+
+            </ul>
+            </li>
+          </ul>
+
+<?php
+}
+?>
+        </div>
+
+      </div>
+    </nav>
+
+    <div class="wide">
+        <div class="tagline">They say, “There is no love sincerer than the love for food”. For us, there is no love sincerer than the love for <br> <font color="red"><strong>INDIAN FOOD!</strong></font><br> At Desi Meals, we serve you our best food with a unique Desi twist. Our one simple mantra: <br><font color="green"><strong><em>“We only serve food that we love to eat.”</em></strong></font></div><br><br><br>
+        <h3 style="color: black",style="text-align:center">Founded in 2021 by - Shubham Darekar - Desi Meals started as a small outlet in a corporate cafeteria. The idea was to serve Irresistible Desi Meals in a convenient, easy-to-carry box. Today, we serve over 1,000+ meals every day across our 100+ outlets in Pimpri-Chinchwad, Pune, Baner, & Bhosari!<br><br> Our 3 pillars of desi taste, tech, and logistics, define our goal to provide a delightful end-to-end experience every single time. Our full-stack approach gives us complete control over all aspects of food delivery from food quality, taste, technology, to last mile delivery. This allows us to provide our customers with a world-class food ordering experience!<br><br> You’ll be spoilt for choices with our unique Desi Box, All-In-1 Meals, Desi Openers, Paratha Wraps, Biryanis, Desserts, & more. We have a wide range of hand-picked, chef-crafted options with a desi mix in anything you pick!<br><br> Superfast Delivery is our forte! We deliver piping hot meals in under 38 mins. with no delivery charges. And did you know, we deliver late in the night till 1 AM? Oh yes, we do! ;)<br><br> So the next time you feel ‘hangry’, remember that Desi Meals are just a few clicks away! :)</h3>
+        <br>
+        <h3 style="color: white">Order food & beverages online from restaurants near & around you. <h3 style="color: white">We deliver food from your neighborhood local joints, your favorite cafes, luxurious & elite restaurants in your area,</h3><h3 style="color : white"> and also from chains like Dominos, KFC, Burger King, Pizza Hut, FreshMenu, Mc Donald's, Subway, Faasos, Cafe Coffee Day, Taco Bell, and more. Exciting bit?</h3><h3 style="color : white"> We place no minimum order restrictions! Order in as little (or as much) as you'd like. We'll Swiggy it to you!</h3></h3><p>&#128516;</p>
+    </div>
+
+         </body>
+</html>
